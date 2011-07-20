cb52378953939091a17b286791a40044f1c4c78c
Josh Allen <jwallen@mit.edu>
2011-07-20 17:23:11 -0400

    Added QuantityField custom model field to pdep app.
    This custom model field provides a nice way of storing a physical
    quantity: a value with associated units. It goes nicely with
    NumberUnitsField and its descendants. Since the value and units of the
    physical quantity are very closely tied together, this combination makes
    a lot of sense. For example, using QuantityField means that you don't
    need two model fields for each physical quantity, which should make the
    implementation that much more straightforward.
    
    The goal in creating QuantityField was to make the ModelForm form
    creation as automatic as possible by moving any needed configuration
    options into the model.

--- rmgweb/main/fields.py
+++ rmgweb/main/fields.py
@@ -35,6 +35,69 @@ from widgets import *
 
 ################################################################################
 
+class QuantityField(models.Field):
+    """
+    A custom model field for representing a physical quantity, with a value
+    and units. In Python this is represented as a tuple of the form
+    ``(value, units)``, where ``value`` is a float and ``units`` is a string.
+    In database tables this data is represented as a text string, with the 
+    value and units separated by a space. When creating a :class:`QuantityField`
+    object, you must specify the `form_class` to use as a form field; normally
+    this will be a class derived from :class:`NumberUnitsField`.
+    """
+    # A brief description for django.contrib.admindocs
+    description = "A physical quantity (value and units)"
+    # Setting the metaclass to SubfieldBase causes to_python() to be invoked 
+    # when fields of this type are initialized
+    __metaclass__ = models.SubfieldBase
+    
+    def __init__(self, form_class, *args, **kwargs):
+        self.form_class = form_class
+        super(QuantityField, self).__init__(*args, **kwargs)
+        
+    def db_type(self, connection):
+        return 'char(50)'
+    
+    def to_python(self, value):
+        """
+        Convert the given `value` from its representation in the database
+        tables to the corresponding Python object.
+        """
+        if (isinstance(value, tuple) and len(value) == 2 and
+            isinstance(value[0], float) and 
+            (isinstance(value[1], str) or isinstance(value[1], unicode))):
+            # We got a value that is already a 2-tuple in the right format, so
+            # simply return it
+            pass
+        elif value != '':
+            # Otherwise assume it is a string and convert it to a 2-tuple
+            tokens = value.split()
+            if len(tokens) != 2:
+                raise ValidationError('Invalid format for quantity.')
+            try:
+                value = (float(tokens[0]),str(tokens[1]))
+            except ValueError:
+                raise ValidationError('Invalid format for quantity.')
+        return value
+    
+    def get_prep_value(self, value):
+        """
+        Convert the given `value` from its representation as a Python object
+        to the corresponding representation in the database tables.
+        """
+        return u'{0} {1}'.format(*value)
+    
+    def formfield(self, **kwargs):
+        """
+        Specify the general form field to use to represent this model field
+        on a ModelForm.
+        """
+        defaults = {'form_class': self.form_class}
+        defaults.update(kwargs)
+        return super(QuantityField, self).formfield(**defaults)
+
+################################################################################
+
 class NumberUnitsField(forms.MultiValueField):
     """
     A field for specifying a numeric value with corresponding units. Use the
